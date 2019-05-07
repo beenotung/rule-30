ca353be1c6a66c3a4e570557dcf098ee58750f3e
Beeno Tung <aabbcc1241@yahoo.com.hk>
2019-05-07 12:40:40 +0800

    fixed for tslint and clean up code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 document.title = 'rule 30';
 
-let style: HTMLStyleElement = document.createElement('style');
+const style: HTMLStyleElement = document.createElement('style');
 style.textContent = `
 body {
   padding: 0;
@@ -10,38 +10,38 @@ body {
 `;
 document.body.appendChild(style);
 
-let canvas: HTMLCanvasElement = document.createElement('canvas');
+const canvas: HTMLCanvasElement = document.createElement('canvas');
 document.body.appendChild(canvas);
-let w = canvas.width = screen.availWidth;
-let h = canvas.height = screen.availHeight;
-let context = canvas.getContext("2d");
-let imageData = context.createImageData(w, h);
+const w = (canvas.width = screen.availWidth);
+const h = (canvas.height = screen.availHeight);
+const context = canvas.getContext('2d');
+const imageData = context.createImageData(w, h);
 
 function render() {
   context.putImageData(imageData, 0, 0);
 }
 
 function setPixel(x, y, isBlack) {
-  let offset = (x + y * w) * 4;
-  let color = isBlack ? 0 : 255;
+  const offset = (x + y * w) * 4;
+  const color = isBlack ? 0 : 255;
   imageData.data[offset + 0] = color;
   imageData.data[offset + 1] = color;
   imageData.data[offset + 2] = color;
   imageData.data[offset + 3] = 255;
 }
 
-let {round} = Math;
-let x = 0;
+const { round } = Math;
 /* x -> y -> isBlack */
-let halfH = round(h / 2);
-let states: number[][] = [[]];
+const halfH = round(h / 2);
+const states: number[][] = [[]];
 states[0][halfH] = 1;
 setPixel(0, halfH, 1);
+let x = 0;
 let startY = halfH;
 let endY = halfH;
 
 /* -1 -> 0 -> 1 -> result */
-let rules: number[][][] = [];
+const rules: number[][][] = [];
 for (let r1 = 0; r1 < 2; r1++) {
   rules[r1] = [];
   for (let r2 = 0; r2 < 2; r2++) {
@@ -58,33 +58,25 @@ rules[0][0][1] = 1;
 
 function getPrevRule(x, y) {
   x--;
-  let s = states[x];
-  return [
-    s[y - 1] || 0,
-    s[y] || 0,
-    s[y + 1] || 0,
-  ]
+  const s = states[x];
+  return [s[y - 1] || 0, s[y] || 0, s[y + 1] || 0];
 }
 
 function getResult(x, y) {
-  let [r1, r2, r3] = getPrevRule(x, y);
+  const [r1, r2, r3] = getPrevRule(x, y);
   return rules[r1][r2][r3];
 }
 
-let isStop = false;
+const isStop = false;
 
 function update() {
-  // if (x === w) {
-  //   isStop = true;
-  //   return
-  // }
   x++;
   startY--;
   endY++;
   states.push([]);
-  let screenX = x % w;
+  const screenX = x % w;
   for (let y = startY; y <= endY; y++) {
-    let isBlack = getResult(x, y);
+    const isBlack = getResult(x, y);
     states[x][y] = isBlack;
     if (y >= 0 && y <= h) {
       setPixel(screenX, y, isBlack);
@@ -93,14 +85,12 @@ function update() {
   render();
 }
 
-// let timer = setInterval(update, 1000 / 30);
-// update();
 function loop() {
   if (isStop) {
-    return
+    return;
   }
   update();
-  requestAnimationFrame(loop)
+  requestAnimationFrame(loop);
 }
 
 loop();
